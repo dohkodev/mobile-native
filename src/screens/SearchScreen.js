94d453eafb90e0ce34d261b467c057ff9b360cc1
Cristopher Vicuña <cvicuna@nectia.com>
2020-05-09 21:05:31 -0400

    agregando http request con axios

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,19 +1,32 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
-import SearchBar from "../components/SearchBar"
+import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
 
   const [term, setTerm] = useState('');
+  const [respose, setResponse] = useState([]);
+
+  const searchApi = async () => {
+    const response = await yelp.get('/search', {
+      params: {
+        limit: 33,
+        term,
+        location: 'san jose'
+      }
+    });
+    setResponse(response.data.businesses);
+  }
 
   return <View>
       <SearchBar 
         term={term} 
         onTermChange={(newTerm) => setTerm(newTerm)}
-        onTermSubmit={() => console.log('asdf')}
+        onTermSubmit={searchApi}
       />
       <Text>Pantalla de busqueda</Text>
-      <Text>{term}</Text>
+      <Text>Hemos encontrado {respose.length} resultados!</Text>
   </View>;
 };
 
